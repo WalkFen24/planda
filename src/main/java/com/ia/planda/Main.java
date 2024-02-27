3a8cfb3b03244ff5b2c1c6610a3b7f825cc860e6
walkfen24 <f.d.walker@outlook.com>
2024-02-26 23:01:14 -0800

    UI update - panda pic test

--- src/main/java/com/ia/planda/Main.java
+++ src/main/java/com/ia/planda/Main.java
@@ -2,10 +2,8 @@ package com.ia.planda;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
 public class Main extends Application {
